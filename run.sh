fdf415a97a838cb2c0e1dbf485068a603a58a9f7
statusrank <435089328@qq.com>
2021-06-15 11:08:36 +0800

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 # !/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail Poly 
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail Exp 
-
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CELoss 
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CBCE 
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CBFOCAL
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail FOCAL 
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CELoss 
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CBCE 
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail CBFOCAL
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail FOCAL 
 CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail SquareAUCLoss
-
-# CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail TPSquareAUCLoss
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail TPSquareAUCLoss
