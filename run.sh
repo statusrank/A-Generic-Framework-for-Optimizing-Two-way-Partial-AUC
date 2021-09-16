94400298bf26830e3e91da0c8979450cd270f0cb
statusrank <37617045+statusrank@users.noreply.github.com>
2021-09-16 10:50:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail SquareAUCLoss
-CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail TPSquareAUCLoss
+CUDA_VISIBLE_DEVICES=0 python3 train.py cifar-10-long-tail TPAUCLoss
